b0a6ac6e994450563c402624a4e4e712e9d6eb18
Oscar Chavarria <ocach14@gmail.com>
2023-11-13 18:19:08 -0600

    Changing folders & adding gtag

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import styles from '@/css/page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <img src="https://images.pexels.com/photos/16361792/pexels-photo-16361792/free-photo-of-seagull-on-beach.jpeg" width={1000} height={500} alt="" />
+    </main>
+  )
+}
